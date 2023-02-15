86d4ff3f79d42d6c984aef1354af5c2a61edf275
pravin0428 <mohite46199@gmail.com>
2023-02-15 16:54:42 +0530

    api added

--- frontend/my-app/src/HttpSevices/posts.js
+++ frontend/my-app/src/HttpSevices/posts.js
@@ -2,38 +2,38 @@ import axios from "axios";
 // import { wait } from "joi-browser";
 
 export const getAllPosts = () => {
-  return axios.get("http://localhost:8080/posts");
+  return axios.get("https://vowel-xj54.onrender.com/posts");
 };
 
 export const getCartData = () => {
-  return axios.get("http://localhost:8080/cart");
+  return axios.get("https://vowel-xj54.onrender.com/cart");
 };
 
 export const getPostsDetailsById = (id) => {
   // console.log(id , "******");
-  return axios.get(`http://localhost:8080/posts/${id}`);
+  return axios.get(`https://vowel-xj54.onrender.com/posts/${id}`);
 };
 
 export const postData = async(creds) => {
   //  console.log(creds,"----------post data")
-  // return axios.post(`http://localhost:8080/posts`, creds);
+  // return axios.post(`https://vowel-xj54.onrender.com/posts`, creds);
 
-  let res = await axios.post("http://localhost:8080/posts" , creds)
+  let res = await axios.post("https://vowel-xj54.onrender.com/posts" , creds)
    return res 
 
 };
 
 export const cartPostData = async(creds) => {
   //  console.log(creds,"----------post data")
-  // return axios.post(`http://localhost:8080/posts`, creds);
+  // return axios.post(`https://vowel-xj54.onrender.com/posts`, creds);
 
-  let res = await axios.post("http://localhost:8080/cart" , creds)
+  let res = await axios.post("https://vowel-xj54.onrender.com/cart" , creds)
    return res 
 
 };
 
 // export const postData = (creds) => {
-//   fetch("http://localhost:8080/posts", {
+//   fetch("https://vowel-xj54.onrender.com/posts", {
 //     method: "POST",
 //     headers: {
 //       "Content-Type": "application/json"
@@ -50,16 +50,16 @@ export const cartPostData = async(creds) => {
 
 
 export const putDataEdit  = async(id , formData) =>{
-  let newData = await axios.put(`http://localhost:8080/posts/${id}`, formData)
+  let newData = await axios.put(`https://vowel-xj54.onrender.com/posts/${id}`, formData)
     return newData
 }
 
 export const deletePost = (id) => {
   console.log(id);
-  return axios.delete(`http://localhost:8080/posts/${id}`);
+  return axios.delete(`https://vowel-xj54.onrender.com/posts/${id}`);
 };
 
 export const deleteCartData = (id) => {
   console.log(id);
-  return axios.delete(`http://localhost:8080/cart/${id}`);
+  return axios.delete(`https://vowel-xj54.onrender.com/cart/${id}`);
 };
